4c85e7705aba1f3189c30f64a6b5733d2bff1c39
Phil Calvin <phil@philcalvin.com>
2013-11-23 20:48:45 -0500

    Load Hops.dylib relatively in the QuickLook bundle

---
+++ Hops/GeneratePreviewForURL.m
@@ -0,0 +1,38 @@
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
+#include <QuickLook/QuickLook.h>
+#import <Foundation/Foundation.h>
+#import "PCArchivePreview.h"
+
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
+void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
+
+/* -----------------------------------------------------------------------------
+   Generate a preview for file
+
+   This function's job is to create preview for designated file
+   ----------------------------------------------------------------------------- */
+
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef request, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options) {
+  PCArchivePreview *preview = [[PCArchivePreview alloc]
+                               initWithURL:(__bridge NSURL *)(url)];
+  @autoreleasepool {
+    NSError *error = nil;
+    BOOL result = [preview generate:&error];
+    if (result) {
+      NSData *data = [preview.plainText dataUsingEncoding:NSUTF8StringEncoding];
+      QLPreviewRequestSetDataRepresentation(request,
+                                            (__bridge CFDataRef)data,
+                                            kUTTypeHTML,
+                                            NULL);
+    } else {
+      NSLog(@"Hops: Unable to generate preview for IPA: %@", error);
+    }
+  }
+  return noErr;
+}
+
+void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
+{
+    // Implement only if supported
+}
